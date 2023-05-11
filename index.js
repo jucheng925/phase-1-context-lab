6c0772917cf31d54a531f99c14e1d56327583583
Julie C <jucheng925@gmail.com>
2023-05-11 11:42:06 -0400

    using context this

--- index.js
+++ index.js
@@ -1,4 +1,50 @@
 /* Your Code Here */
+const exampleArray = ["Kim","Smith","clerk",8]
+function createEmployeeRecord(array) {
+    const employeeRecord = {
+        firstName: array[0],
+        familyName: array[1],
+        title: array[2],
+        payPerHour: array[3],
+        timeInEvents: [],
+        timeOutEvents: [],
+    }
+    return employeeRecord
+}
+function createEmployeeRecords(arrayOfArray) {
+    return arrayOfArray.map(createEmployeeRecord)
+}
+
+//employee record use as context
+function createTimeInEvent(time){
+    const timeObj = {
+        type: "TimeIn",
+        hour: parseInt(time.slice(11), 10),
+        date: time.slice(0,10),
+    }
+    this.timeInEvents.push(timeObj)
+    return this
+}
+
+function createTimeOutEvent(time) {
+    const timeObj = {
+        type: "TimeOut",
+        hour: parseInt(time.slice(11), 10),
+        date: time.slice(0,10),
+    }
+    this.timeOutEvents.push(timeObj)
+    return this
+}
+
+function hoursWorkedOnDate(soughtDate) {
+    const compareTimeIn = this.timeInEvents.find(element => element.date === soughtDate);
+    const compareTimeOut = this.timeOutEvents.find(element => element.date === soughtDate);
+    return (compareTimeOut.hour - compareTimeIn.hour) / 100
+}
+
+function wagesEarnedOnDate(soughtDate) {
+    return hoursWorkedOnDate.call(this, soughtDate) * this.payPerHour
+}
 
 /*
  We're giving you this function. Take a look at it, you might see some usage
@@ -21,3 +67,14 @@ const allWagesFor = function () {
     return payable
 }
 
+function findEmployeeByFirstName(srcArray, findFirstName) {
+    return srcArray.find(e => e.firstName === findFirstName)
+}
+
+function calculatePayroll(srcArray) {
+    const payable = srcArray.reduce(function(accumulator, object) {
+        return accumulator + allWagesFor.call(object)
+    }.bind(this),0)
+    
+    return payable
+}
